a6ab70db1b3f7e259e261d73a181c28a100cfdcd
Javier <gutifer666@gmail.com>
2025-06-01 23:50:46 +0200

    Add Programacion module with basic structure and initial content
    - Created `Programacion` module containing `programacion`, `ut1-prog` components.
    - Added templates (`HTML`) and styles (`CSS`) with placeholder content and structure.
    - Updated the module with Angular component boilerplate.

---
+++ src/app/section/module/programacion/ut1-prog/ut1-prog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ut1-prog',
+  imports: [],
+  templateUrl: './ut1-prog.html',
+  styleUrl: './ut1-prog.css'
+})
+export class Ut1Prog {
+
+}
