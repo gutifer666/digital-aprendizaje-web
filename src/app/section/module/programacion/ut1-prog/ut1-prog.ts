b0d26e911058c0dabfe8c052283ee42fb68d25f1
Javier <gutifer666@gmail.com>
2025-06-02 19:39:57 +0200

    Integrate `NgOptimizedImage` and clean up unused images in `ut1-prog` template
    - Added `NgOptimizedImage` to the component imports for improved image loading performance.
    - Removed numerous unused images and their references in the `ut1-prog.html` template to clean up the codebase.

--- src/app/section/module/programacion/ut1-prog/ut1-prog.ts
+++ src/app/section/module/programacion/ut1-prog/ut1-prog.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-ut1-prog',
-  imports: [],
+    imports: [
+        NgOptimizedImage
+    ],
   templateUrl: './ut1-prog.html',
   styleUrl: './ut1-prog.css'
 })
