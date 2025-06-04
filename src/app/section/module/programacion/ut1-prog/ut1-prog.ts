71143552c7f7ae8b23f59260cd1ff611c035a7d3
Javier <gutifer666@gmail.com>
2025-06-04 15:34:58 +0200

    added ut programming components

--- src/app/section/module/programacion/ut1-prog/ut1-prog.ts
+++ src/app/section/module/programacion/ut1-prog/ut1-prog.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-ut1-prog',
     imports: [
-        NgOptimizedImage
     ],
   templateUrl: './ut1-prog.html',
   styleUrl: './ut1-prog.css'
